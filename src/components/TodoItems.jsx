8d6d93d3e775fb95ff46065a3ed9b877a756eed7
Yuri Mikushov <mikushov.yu@gmail.com>
2021-03-06 12:16:44 +0300

    feat: added message for empty todo list

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import TodoItem from './TodoItem'
 
-const TodoItems = ({ todos, deleteTodo }) => {
-  return (
-    <div className='todo-items block'>
-      {todos.map((todo) => (
+const TodoItems = ({ todos, deleteTodo }) => (
+  <div className='todo-items block'>
+    {todos.length > 0 ? (
+      todos.map((todo) => (
         <TodoItem
           key={todo.id}
           id={todo.id}
           title={todo.title}
           deleteTodo={() => deleteTodo(todo.id)}
         />
-      ))}
-    </div>
-  )
-}
+      ))
+    ) : (
+      <div className='dont-have-todos-message has-text-grey-light'>
+        You don&apos;t have any todo
+      </div>
+    )}
+  </div>
+)
 
 TodoItems.propTypes = {
   todos: PropTypes.arrayOf(
