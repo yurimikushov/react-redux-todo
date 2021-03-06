af508322d637372057f72b416f59412f41172242
Yuri Mikushov <mikushov.yu@gmail.com>
2021-03-06 20:53:03 +0300

    chore: changed app title

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TodoHeader = () => (
-  <header className='todo-header title is-1'>Todo list</header>
+  <header className='todo-header title is-1'>Todo</header>
 )
 
 export default TodoHeader
